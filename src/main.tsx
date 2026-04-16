6a8a0028e022b544c8ee2efa224a014c8ed283c6
pro100juneor <pro100juneor@gmail.com>
2026-04-16 20:28:42 +0200

    feat: src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import { Toaster } from "@/components/ui/toaster";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthProvider>
       <App />
+      <Toaster />
     </AuthProvider>
   </BrowserRouter>
 );
