02414fdc00cda7902807bd24776099e8a2c58f39
pro100juneor <pro100juneor@gmail.com>
2026-04-16 09:35:44 +0200

    fix:src/components/ui/badge.tsx

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+const badgeVariants = cva("inflex-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold",{variants:{variant:{default:"border-transparent bg-primary text-primary-foreground",secondary:"border-transparent bg-secondary text-secondary-foreground",destructive:"border-transparent bg-destructive text-destructive-foreground",outline:"text-foreground"}},defaultVariants:{variant:"default"}});export function Badge({className,variant,...props}:any){return<div className={cn(badgeVariants({variant}),className)}{...props}/>;}export{badgeVariants};
